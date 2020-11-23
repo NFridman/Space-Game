8b4b340743a2fe46bdbc74a22f47cbc9c3119fdf
n.fridman <n.fridman@goto-apps.com>
2020-11-23 21:49:06 +0300

    Added camera rotation
    Камера вращается при перемещении мыши с зажатой клавишей ЛКМ.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("Components")]
+    [Tooltip("Main camera transform component")]
+    [SerializeField] private Transform m_MainCameraTransform;
+    
+    [Header("Settings")]
+    [Tooltip("Rotation speed")]
+    public int speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            var x = -Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+            var y = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+            var eulerX = (m_MainCameraTransform.eulerAngles.x + y) % 360;
+            var eulerY = (m_MainCameraTransform.rotation.eulerAngles.y + x) % 360;
+            m_MainCameraTransform.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+        }
+    }
+}
